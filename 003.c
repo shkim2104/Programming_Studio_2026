bf97c907f332447d5440d14a2e840a1a55076fb5
shkim2104 <eversdky2104@gmail.com>
2026-03-07 21:19:47 +0900

    주석추가

--- 003.c
+++ 003.c
@@ -62,9 +62,9 @@ int findRoom(int persons[5]){
     t = 4;
 
     while(1){
-        r = rand() %(t - n + 1) + n;
+        r = rand() %(t - n + 1) + n; // 방개수를 1 ~ 5개 까지 랜덤으로 배정
         
-        if(persons[r] < 2){
+        if(persons[r] < 2){ // 방에 2명이 다 찼다면 n+1 호로 배
             return r + 1;  
         }
     }
@@ -80,7 +80,7 @@ void printReport(char mn[10][20], int mr[10], int mc, char wn[10][20], int wr[10
     printf("남학생 명단 %d\n\n" , mc); 
 
     for(int i =0; i< mc; i++){
-        printf("%d. %s [10%d]\n" , i+1 ,mn[i] , mr[i]);
+        printf("%d. %s [10%d]\n" , i+1 ,mn[i] , mr[i]); // 남학생의 방과 호수 이름을출력 남자가 있는 수만큼
     }
 
     printf("\n");
@@ -88,7 +88,7 @@ void printReport(char mn[10][20], int mr[10], int mc, char wn[10][20], int wr[10
     printf("여학생 명 %d\n" , wc);
 
     for(int i=0; i < wc;i++){
-        printf("%d. %s [20%d]\n" , i+1 , wn[i] , wr[i]);
+        printf("%d. %s [20%d]\n" , i+1 , wn[i] , wr[i]); // 남학생과 마찬가지로 출력
     }
 
     printf("\n");
@@ -99,7 +99,7 @@ void printReport(char mn[10][20], int mr[10], int mc, char wn[10][20], int wr[10
 
     for(int i =0; i < 5; i++){
         printf("10%d호 : " , i+1);
-        for(int j =0; j <mc; j++){
+        for(int j =0; j <mc; j++){      //  아래 여학생과 마찬가지로 동일함
             if(mr[j] ==i+1){
                 printf("%s " , mn[j]);
             }
@@ -109,7 +109,7 @@ void printReport(char mn[10][20], int mr[10], int mc, char wn[10][20], int wr[10
 
     for(int i = 0; i < 5; i++){
         printf("20%d호 : " , i+1);
-        for(int j =0; j <wc; j++){
+        for(int j =0; j <wc; j++){  // 여학생이 몇호에 있는지  5개 방만큼 배열을 wc만큼 반복하여 출력함 
             if(wr[j] ==i+1){
                 printf("%s " , wn[j]);
             }
@@ -121,24 +121,23 @@ void printReport(char mn[10][20], int mr[10], int mc, char wn[10][20], int wr[10
 
 void ins_studata(int g){
     int room;
-
-    
+        
     if(g == 1){ 
         printf("학생 이름은? > ");
-        scanf("%s" , mn[mc]);
+        scanf("%s" , mn[mc]);           //배열에 이름을 기입함
 
-        room = findRoom(mp);
+        room = findRoom(mp);    //방을 지정된 난수로 배정을 받아서 mr 배열에 추가하고
         mr[mc] = room; 
         mp[room -1] = mp[room -1] + 1; 
 
-        printf("%s 학생 10%d호실 배정되었습니다\n" , mn[mc] , room);
-        mc = mc + 1;
+        printf("%s 학생 10%d호실 배정되었습니다\n" , mn[mc] , room);  
+        mc = mc + 1;            // 한명을 추가할때마다 카운트를 더한다
     }    
     
     else if(g ==2){
         printf("학생 이름은? > ");
         scanf("%s" , wn[wc]);
-        room = findRoom(wp);
+        room = findRoom(wp);            //여학생도 마찬가지로 추가한다
         wr[wc] = room;
         wp[room -1] = wp[room -1] + 1;
 
